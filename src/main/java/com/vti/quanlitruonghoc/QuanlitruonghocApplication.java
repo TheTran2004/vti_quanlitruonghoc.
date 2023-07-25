9b2650cfd954d1989883af8dd26a14533fd0d920
ANOS_TMT\trant <tranthep363184700@gmail.com>
2023-07-25 14:44:02 +0700

    Fist commit

---
+++ src/main/java/com/vti/quanlitruonghoc/QuanlitruonghocApplication.java
@@ -0,0 +1,13 @@
+package com.vti.quanlitruonghoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlitruonghocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlitruonghocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/quanlitruonghoc/QuanlitruonghocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.quanlitruonghoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlitruonghocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
